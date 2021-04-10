f37463e9c5614444ed5bc22c650af7bf3c34ba83
Moonwalker <abhisk.mhra@gmail.com>
2021-04-10 23:17:17 +0530

    Plugged in the JS file to make the translation work via dothraki translate api

---
+++ app.js
@@ -0,0 +1,39 @@
+var transButton = document.querySelector("#click-btn");
+var textInput = document.querySelector("#text-input");
+
+
+var output = document.querySelector("#output");
+
+var serverUrl = "	https://api.funtranslations.com/translate/dothraki.json";
+
+function getTransUrl(text) {
+    return serverUrl + "?" + "text=" + text;
+}
+
+
+function errorHandler(err) {
+    console.log("error occurred: " + err);
+    alert("Something wrong with Server");
+}
+
+transButton.addEventListener("click", function click() {
+
+    // taking input
+    var inputText = textInput.value;
+
+    // calling the server
+    fetch(getTransUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var transText = json.contents.translated;
+            output.innerHTML = transText;
+        } )
+        .catch(errorHandler)
+
+        
+
+    // console.log("Clicked");
+
+    // output.innerHTML = textInput.value;
+
+});
